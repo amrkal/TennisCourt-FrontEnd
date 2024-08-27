c0db970c0af0cbfc90ad17cae8945e7ed0b43d03
AmrKal <a.k251198@hotmail.com>
2024-08-27 15:37:05 +0300

    Admin FOlder

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,12 @@
 import React from 'react';
 import { Route, Routes as Switch } from 'react-router-dom';
 import App from './App'; // Example component
-import LoginPage from './LoginPage'; // Example component
-import AdminPage from './AdminPage'; // Example component
+// import LoginPage from './LoginPage'; // Example component
+// import AdminPage from './AdminPage'; // Example component
 
 const Routes = () => (
   <Switch>
     <Route path="/" element={<App />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/admin" element={<AdminPage />} />
   </Switch>
 );
 
